d460dc44350ef3c381f25aad68dedb1625bb7bfe
Oleh Voloshyn <bazisc.qa@gmail.com>
2024-05-25 14:10:56 +0300

    update .env.example and config validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,6 @@ import Joi from 'joi';
       isGlobal: true,
       validationSchema: Joi.object({
         NODE_ENV: Joi.valid('development', 'production', 'test').required(),
-        CLIENT_URL: Joi.string().required(),
         HTTP_HOST: Joi.string().required(),
         HTTP_PORT: Joi.number().required(),
         AUTH_SVC_URL: Joi.string().required(),
